e557304378f7aa53f260b96b1fff2aafbdf63d64
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-11-10 13:45:18 +0100

    + 'UserId'

--- src/app/auth-api/auth.state.ts
+++ src/app/auth-api/auth.state.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Action, State, StateContext } from '@ngxs/store';
 import { LoginSuccess, Logout } from 'src/app/auth-api/auth.actions';
+import { decodeJwt } from 'jose';
 
 export interface AuthStateModel {
+  UserId: string;
   AccessToken: string;
   RefreshToken: string;
   IdToken: string;
@@ -11,6 +13,7 @@ export interface AuthStateModel {
 @State<AuthStateModel>({
   name: 'auth',
   defaults: {
+    UserId: '',
     AccessToken: '',
     RefreshToken: '',
     IdToken: '',
@@ -20,7 +23,10 @@ export interface AuthStateModel {
 export class AuthState {
   @Action(LoginSuccess)
   login(ctx: StateContext<AuthStateModel>, action: LoginSuccess) {
+    let payload = decodeJwt(action.successfulLogin.AccessToken);
+
     ctx.setState({
+      UserId: payload.sub || '',
       AccessToken: action.successfulLogin.AccessToken,
       RefreshToken: action.successfulLogin.RefreshToken,
       IdToken: action.successfulLogin.IdToken,
@@ -30,6 +36,7 @@ export class AuthState {
   @Action(Logout)
   logout(ctx: StateContext<AuthStateModel>, action: Logout) {
     ctx.setState({
+      UserId: '',
       AccessToken: '',
       RefreshToken: '',
       IdToken: '',
