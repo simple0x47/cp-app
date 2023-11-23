5a9d97c7d466926e88093fbb32370ecb6e6349cc
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2023-11-23 15:26:10 +0100

    + logout button

--- src/app/dashboard/toolbar/toolbar.component.ts
+++ src/app/dashboard/toolbar/toolbar.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {AuthService} from "../../auth-api/auth.service";
+import {Router} from "@angular/router";
+import {RoutingService} from "../../routing/routing.service";
 
 @Component({
   selector: 'app-toolbar',
@@ -6,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./toolbar.component.css']
 })
 export class ToolbarComponent {
+  public constructor(
+    private _authService: AuthService,
+    private _routingService: RoutingService
+  ) {
+  }
 
+  public logout() {
+    this._authService.logout();
+    this._routingService.goToLogin();
+  }
 }
